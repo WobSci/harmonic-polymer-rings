790c06948445fab64e159ee23c2d86441e8ef3a0
Kevin Lindt <kevin.lindt@outlook.com>
2025-02-01 14:54:36 +0100

    Implement basic plot of MSD vs t_norm

--- MSD-of-ring-segments.py
+++ MSD-of-ring-segments.py
@@ -1,18 +1,20 @@
 import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
 
 # Definitions of Scalars
-b = 2
+b = 1
 N = 100
 R = 1
 
 # Definitions of Arrays
-t_norm = np.logspace(1, 12, num=10)
+t_norm = np.logspace(1, 12, num=100)
 p = np.linspace(1, np.floor(N/2), int(np.floor(N/2)))
 print(p)
 
-# Calculation
+# Calculate MSD for t_norm values
 A = 6/(3*np.pi) * b**2/N # MSD of center of mass
-B = ((4*np.pi**2*p**2)/(N*b**2)+N/R**2)**-1
+B = ((4*np.pi**2*p**2)/(N*b**2)+N/R**2)**(-1)
 C = (2*p/N)**2 + 1/np.pi**2 * b**2/R**2
 
 MSD = []
@@ -21,7 +23,15 @@ for i in t_norm:
     MSD.append(temp)
 
 MSD = np.array(MSD)
-print(MSD)
 
-#MSD = np.sum(B*1-np.e**(-C*t_norm))
-#print(MSD)
+# Plot MSD vs t_norm
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=t_norm, y=MSD, mode='markers+lines'))
+fig.update_layout(
+    title='MSD of ring segments',
+    xaxis_title='t/tau_s',
+    yaxis_title='MSD'
+)
+fig.update_xaxes(type="log")
+fig.update_yaxes(type="log")
+fig.show()
