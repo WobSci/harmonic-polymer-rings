efe382b68e768121657ea7dc7e4654166350c402
Kevin Lindt <kevin.lindt@outlook.com>
2025-02-01 13:33:08 +0100

    Implement loop to calculate MSD for time points

--- MSD-of-ring-segments.py
+++ MSD-of-ring-segments.py
@@ -11,12 +11,17 @@ p = np.linspace(1, np.floor(N/2), int(np.floor(N/2)))
 print(p)
 
 # Calculation
-A = 6/(3*np.pi) * b**2/N * t_norm # MSD of center of mass
-print(A)
-
+A = 6/(3*np.pi) * b**2/N # MSD of center of mass
 B = ((4*np.pi**2*p**2)/(N*b**2)+N/R**2)**-1
 C = (2*p/N)**2 + 1/np.pi**2 * b**2/R**2
-print(C)
+
+MSD = []
+for i in t_norm:
+    temp = A*i + 4*np.sum(B*(1-np.e**(-C*i)))
+    MSD.append(temp)
+
+MSD = np.array(MSD)
+print(MSD)
 
 #MSD = np.sum(B*1-np.e**(-C*t_norm))
 #print(MSD)
