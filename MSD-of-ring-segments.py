ef24832b19ad33b06fc5ed73a862f609182a83b1
Kevin Lindt <kevin.lindt@outlook.com>
2025-02-01 18:15:41 +0100

    Add annotation of rms rdius of gyration into the plot and adjust plot appearance further

--- MSD-of-ring-segments.py
+++ MSD-of-ring-segments.py
@@ -4,13 +4,12 @@ import plotly.graph_objects as go
 
 # Definitions of Scalars
 b = 11*10**(-10)
-N = 100
-R = 1*10**(-9)
+N = 5000
+R = 25*10**(-9)
 
 # Definitions of Arrays
-t_norm = np.logspace(0, 14, num=100)
+t_norm = np.logspace(0, 8, num=100)
 p = np.linspace(1, np.floor(N/2), int(np.floor(N/2)))
-print(p)
 
 # Calculate MSD for t_norm values
 A = 6/(3*np.pi**2) * b**2/N # MSD of center of mass
@@ -26,20 +25,22 @@ MSD = np.array(MSD)
 
 # Plot MSD vs t_norm
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=t_norm, y=MSD, mode='markers+lines', name=r'$\frac{6}{3\pi^2}\frac{b^2}{N}\frac{t}{\tau_s}+4\sum\limits_{p=1}^{N/2}\left[\left(\frac{4\pi^2p^2}{Nb^2}+\frac{N}{R^2}\right)^{-1}\left(1-e^{-\left(\left(\frac{2p}{N}\right)^2+\left(\frac{b}{\pi R}\right)^2\right)\frac{t}{\tau_s}}\right)\right]$'))
+fig.add_trace(go.Scatter(x=t_norm, y=MSD, mode='markers+lines', name=r'$\left\langle\left(\vec{r}_n\left(t\right)-\vec{r}_n\left(0\right)\right)^2\right\rangle = \frac{6}{3\pi^2}\frac{b^2}{N}\frac{t}{\tau_s}+4\sum\limits_{p=1}^{N/2}\left[\left(\frac{4\pi^2p^2}{Nb^2}+\frac{N}{\tilde{R}^2}\right)^{-1}\left(1-e^{-\left(\left(\frac{2p}{N}\right)^2+\left(\frac{b}{\pi \tilde{R}}\right)^2\right)\frac{t}{\tau_s}}\right)\right]$'))
 fig.update_layout(
-    title=f"b = {b*10**9} nm, R = {R*10**9} nm, N = {N}",
+    title=fr'$b = {b*10**9:.2g} \text{{ nm, }} \tilde{{R}} = {R*10**9:.2g} \text{{ nm, }} N = {N}$',
     xaxis_title=r'$t/\tau_s$',
-    yaxis_title='MSD',
+    yaxis_title=r'$\left\langle\left(\vec{r}_n\left(t\right)-\vec{r}_n\left(0\right)\right)^2\right\rangle$',
     xaxis=dict(tickformat='.0e'),
-    yaxis=dict(tickformat='.0e')
+    yaxis=dict(tickformat='.0e'),
+    legend_font_size=16,
+    title_font_size=20
 )
 fig.update_xaxes(type="log")
 fig.update_yaxes(type="log")
 
 # Calculate characteristic time points
 RMS_ROG = R*b/np.pi * (np.arctan(np.pi*R/b)-np.arctan(2/N*np.pi*R/b))
-print(RMS_ROG)
+
 t_char0 = 1
 t_char1 = 4*np.pi**2*RMS_ROG**2/b**4
 t_char2 = 6*np.pi**2*RMS_ROG*N/b**2
@@ -74,7 +75,13 @@ for i in t_regime2:
     MSD_regime2.append(temp)
 
 # Add approximations to plot
-fig.add_trace(go.Scatter(x=t_regime0, y=MSD_regime0, mode='lines', name=r'$\left\langle\left(\vec{r}_n\left(t\right)-\vec{r}_n\left(0\right)\right)\right\rangle\approx \frac{2b^2}{\pi^{3/2}}\left(\frac{t}{\tau_s}\right)^{1/2}$', line=dict(dash='dot', color='green', width=2)))
-fig.add_trace(go.Scatter(x=t_regime1, y=MSD_regime1, mode='lines', name=r'$\left\langle\left(\vec{r}_n\left(t\right)-\vec{r}_n\left(0\right)\right)\right\rangle\approx 2\left\langle R_g^2\right\rangle_{eq}$', line=dict(dash='dash', color='green', width=2)))
-fig.add_trace(go.Scatter(x=t_regime2, y=MSD_regime2, mode='lines', name=r'$\left\langle\left(\vec{r}_n\left(t\right)-\vec{r}_n\left(0\right)\right)\right\rangle\approx \frac{6}{3\pi^2}\frac{b^2}{N}\frac{t}{\tau_s}$', line=dict(dash='solid', color='green', width=2)))
+fig.add_trace(go.Scatter(x=t_regime0, y=MSD_regime0, mode='lines', name=r'$\left\langle\left(\vec{r}_n\left(t\right)-\vec{r}_n\left(0\right)\right)^2\right\rangle\approx \frac{2b^2}{\pi^{3/2}}\left(\frac{t}{\tau_s}\right)^{1/2}$', line=dict(dash='dot', color='green', width=2)))
+fig.add_trace(go.Scatter(x=t_regime1, y=MSD_regime1, mode='lines', name=r'$\left\langle\left(\vec{r}_n\left(t\right)-\vec{r}_n\left(0\right)\right)^2\right\rangle\approx 2\left\langle R_g^2\right\rangle_{eq}$', line=dict(dash='dash', color='green', width=2)))
+fig.add_trace(go.Scatter(x=t_regime2, y=MSD_regime2, mode='lines', name=r'$\left\langle\left(\vec{r}_n\left(t\right)-\vec{r}_n\left(0\right)\right)^2\right\rangle\approx \frac{6}{3\pi^2}\frac{b^2}{N}\frac{t}{\tau_s}$', line=dict(dash='solid', color='green', width=2)))
+fig.add_annotation(
+    x=np.log10(np.median(t_norm)), y=np.log10(0.75*np.max(MSD)),
+    text=fr'$\left\langle R_g^2\right\rangle_{{eq}}=\frac{{\tilde{{R}}b}}{{\pi}}\left(\arctan\left(\frac{{\pi\tilde{{R}}}}{{b}}\right)-\arctan\left(\frac{{2\pi\tilde{{R}}}}{{Nb}}\right)\right)={RMS_ROG:.3g} \text{{ nm}}^2$',
+    showarrow=False,
+    font=dict(size=20)
+)
 fig.show()
